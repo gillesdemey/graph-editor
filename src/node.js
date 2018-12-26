2503e8a26f5b8d7228f6abbe7d7344d9393ec5c8
Gilles De Mey <gilles.de.mey@gmail.com>
2018-12-26 17:07:04 +0100

    feat: adds icon rectangle

--- src/node.js
+++ src/node.js
@@ -1,4 +1,5 @@
 import { Group, Rect, Text } from 'konva'
+import { shade } from 'polished'
 import { getBezierPoints, stopPropagation } from './utils'
 
 class GraphNode extends Group {
@@ -10,7 +11,7 @@ class GraphNode extends Group {
     super(Object.assign(defaults, options))
 
     this._data = details
-    const { id, label, color, textColor, states } = details
+    const { id, label, color = '#ecf0f1', textColor = '#7f8c8d', states, icon } = details
 
     this.connections = new Set()
     this.contentGroup = new Group()
@@ -52,35 +53,52 @@ class GraphNode extends Group {
   }
 
   drawLabel (label, { color, textColor }) {
+    const HEIGHT = 40
+    const ICON_SIZE = HEIGHT
+    const PADDING = 15
+
     const { contentGroup } = this
 
     const text = new Text({
       text: label,
       fontSize: 15,
       fontFamily: 'monospace',
-      fill: textColor || '#7f8c8d',
+      fill: textColor,
       align: 'center',
-      height: 40,
-      padding: 15,
+      height: HEIGHT,
+      padding: PADDING,
       opacity: 0.9
     })
 
     text.offset({
-      x: text.width() / 2,
+      x: (text.width() / 2) - (ICON_SIZE / 3),
       y: text.height() / 2
     })
 
     const rect = new Rect({
-      fill: color || '#ecf0f1',
-      width: text.width(),
-      height: text.height(),
+      fill: color,
+      width: text.width() + ICON_SIZE,
+      height: HEIGHT,
       cornerRadius: 6,
       offset: {
-        x: text.width() / 2,
+        x: (text.width() / 2) + (ICON_SIZE / 2),
         y: text.height() / 2
       }
     })
-    this.drawHandles(rect)
+
+    const iconRect = new Rect({
+      width: HEIGHT,
+      height: HEIGHT,
+      fill: shade(0.1, color),
+      cornerRadius: 6,
+      x: -(rect.width() / 2),
+      y: -(HEIGHT / 2)
+    })
+
+    const labelGroup = new Group()
+    labelGroup.add(rect, iconRect, text)
+
+    this.drawHandles(labelGroup)
 
     text.on('mouseenter', () => {
       contentGroup.getStage().container().style.cursor = 'move'
@@ -90,22 +108,25 @@ class GraphNode extends Group {
       contentGroup.getStage().container().style.cursor = 'default'
     })
 
-    contentGroup.add(rect, text)
+    contentGroup.add(rect, iconRect, text)
   }
 
-  drawHandles (rect) {
+  drawHandles (bounds) {
     const HANDLE_SIZE = 10
+    const FILL = '#95a5a6'
+
     const { contentGroup } = this
+    const { width } = bounds.getClientRect()
 
     const leftHandle = new Rect({
       name: 'leftHandle',
       width: 10,
       height: 10,
       cornerRadius: 3,
-      x: -(rect.width() / 2),
+      x: -(width / 2),
       offsetX: HANDLE_SIZE / 2,
       offsetY: HANDLE_SIZE / 2,
-      fill: '#95a5a6'
+      fill: FILL
     })
 
     const rightHandle = new Rect({
@@ -113,10 +134,10 @@ class GraphNode extends Group {
       width: 10,
       height: 10,
       cornerRadius: 3,
-      x: rect.width() / 2,
+      x: width / 2,
       offsetX: HANDLE_SIZE / 2,
       offsetY: HANDLE_SIZE / 2,
-      fill: '#95a5a6'
+      fill: FILL
     })
 
     // disable drag when initiated from a handle — so we can join nodes
